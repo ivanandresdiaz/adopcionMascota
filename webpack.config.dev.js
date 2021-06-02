7094f9f0683e9abd3d51b82e6291e1a1182e2387
Andres Diaz <ivanandresdilox@gmail.com>
2021-06-02 17:49:36 -0500

    fuente Nunito agregada

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -35,6 +35,20 @@ module.exports={
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
         type:'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        }
       }
     ]
   },
